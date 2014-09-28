caf8c2ba4c4d0bf06443aab70d6a1a0280dc9891
SeedPaths-User <lessdismissive@email.com>
2014-09-28 04:29:51 -0600

    Htmling and cssing.
    321 files committed to master. Damn son.

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Conact/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.Contact());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Contact contact)
+        {
+            Models.ContactReportEntities db = new Models.ContactReportEntities();
+
+            db.Contacts.Add(contact);
+
+            db.SaveChanges();
+
+            return RedirectToAction("ThankYou", "Contact");
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Portfolio/Portfolio/Models/ContactValidation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// step 1: Import System.ComponentModel.DataAnnotations
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+
+    //step 2: create a partial class for the class we're trying to be the validation class
+    //step 3. Add the annotation for the metaDataType
+    [MetadataType(typeof(ContactValidation))]
+    public partial class Contact
+    {
+    }
+    public class ContactValidation
+    {
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+        [Required, EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public string phoneNum { get; set; }
+        [Required]
+        public string comment { get; set; }
+    }
+}
